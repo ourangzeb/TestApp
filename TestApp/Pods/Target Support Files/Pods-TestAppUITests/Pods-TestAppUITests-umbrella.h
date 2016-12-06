6982175fe17e1db265da801b5d93610bce0de9bf
Ourang Zaib <khanourangzaib@gmail.com>
2016-12-06 22:46:56 +0500

    pod installation of frameworks
    Alamofire
    KingFisher
    ProgressHUD

---
+++ TestApp/Pods/Target Support Files/Pods-TestAppUITests/Pods-TestAppUITests-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_TestAppUITestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_TestAppUITestsVersionString[];
+
